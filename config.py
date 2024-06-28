97be9c47eb89fab23856880bcf514311d46ed8d8
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-06-28 15:03:59 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,10 @@ MODEL_FOLDER = ROOT / 'models' / 'yolov10'
 # Source parameters
 INPUT_VIDEO = '00000002761000000_new.mp4'
 OUTPUT_NAME = '00000002761000000_new_vTest'
+
+# Zone parameters
+REAL_WIDTH = 730 # cm
+REAL_HEIGHT = 5000 # cm
 JSON_NAME = '00000002761000000_new_region.json'
 
 # Deep Learning model configuration
@@ -17,4 +21,4 @@ MODEL_WEIGHTS = 'yolov10b.pt'
 # Inference configuration
 CLASS_FILTER = [0,1,2,3,5,7]
 IMAGE_SIZE = 640
-CONFIDENCE = 0.1
+CONFIDENCE = 0.5
